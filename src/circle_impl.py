7e013bb74bf02181442ae119d5eb7b0b69ffc65f
Rahul Bhadani <rahulbhadani@email.arizona.edu>
2019-12-15 15:15:26 -0700

    Simulation Example changes

--- src/circle_impl.py
+++ src/circle_impl.py
@@ -5,8 +5,9 @@
 # All rights reserved.
 
 from circle import circle
-import Bagplot
+from Bagplot import plot_timeseries
 from Bagplot import Bagplot
+from GZStats import GZStats
 import signal
 import matlab.engine
 import pandas as pd
@@ -20,47 +21,63 @@ from operator import add
 
 Files = []
 i = 1
-NUM_VEHICLES  = [5, 20]
-UPDATE_RATE = [1]
 
-# A set of six simulation with two cars and update rate varying from 20 to 120 Hz
-for nn in NUM_VEHICLES: 
-    for i in UPDATE_RATE:
-        print("===================================")
-        print("========   Simulation  Begin   ========")
-        print("===================================")
 
-        ## Simulation 1
-        # Define Simulation Configuration
-        simConfig = {"circumference": 260.0, "num_vehicle":  nn, "update_rate": i, "log_time": 90.0, "max_update_rate": 100.0, "time_step": 0.01}
-        #cl = circle(simConfig["circumference"], simConfig["num_vehicle"])
-        Circ = circle(**simConfig)
-        #Print the X coordinates of all vehicles for sanity checking
-        print("X coordinates of vehicles spawned: ",  Circ.X)
+print("===================================")
+print("======   Simulation  Begin   =======")
+print("===================================")
 
-        #Print the Y coordinates of all vehicles for sanity checking
-        print("Y coordinates of vehicles spawned: ",  Circ.Y)
+NUM_VEHICLES  = [5, 15, 20, 30]
+UPDATE_RATE = [0.5, 1, 10, 25, 50, 100]
+MAX_UPDATE_RATE = [25, 50, 100] # This is for Gazebo
 
-        # Start a simulation
-        bagFile = Circ.start_circle_sim()
 
-        if bagFile is not None:
-            Bag  = Bagplot(bagFile)
-            datafiles = Bag.getDataFile(fileFilter="magna-setvel", msg_types = "odom")
-            Files.append(datafiles)
-            Bag.plot_timeseries(datafiles, 'PoseY', fileFilter='magna-setvel')
+NUM_VEHICLES  = [5]
+UPDATE_RATE = [0.25]
+MAX_UPDATE_RATE = [25] # This is for Gazebo
 
-        configFileToSave =  Circ.bagfile[0:-4] + "/" + "simConfig.yaml"
 
-        with open(configFileToSave, 'w') as file:
-            documents = yaml.dump(simConfig, file)
-    
-        time.sleep(7)
+
+for num_vehicles in NUM_VEHICLES:
+    for update_rate in  UPDATE_RATE:
+        for max_update_rate in MAX_UPDATE_RATE:
+            ## Simulation 1
+            # Define Simulation Configuration
+            simConfig = {"circumference": 450.0, "num_vehicle":  num_vehicles, "update_rate": update_rate, "log_time": 120.0, "max_update_rate": max_update_rate, "time_step": 0.01}
+            #cl = circle(simConfig["circumference"], simConfig["num_vehicle"])
+            Circ = circle(**simConfig)
+
+            # Start a simulation
+            bagFile = Circ.start_circle_sim()
+
+            gz_stat_file = Circ.gzstatsFile
+
+            GZ = GZStats(gz_stat_file)
+            GZ.plotRTF()
+            GZ.plotSimStatus()
+
+            if bagFile is not None:
+                Bag  = Bagplot(bagFile)
+                datafiles = Bag.getDataFile(fileFilter="magna-setvel", msg_types = "odom")
+                Files.append(datafiles)
+                Bag.plot_timeseries(datafiles, 'PoseY', fileFilter='magna-setvel')
+                Bag.plot_topic_hz(datafiles)
+
+            configFileToSave =  Circ.bagfile[0:-4] + "/" + "simConfig.yaml"
+
+            with open(configFileToSave, 'w') as file:
+                documents = yaml.dump(simConfig, file)
+
+            time.sleep(6)
+            pt.close('all')
 
 # Now since we have all the bag files
 
 Files = reduce(add, Files)
 
-Bagplot.plot_timeseries(Files, 'PoseY', Title='Consolidated Plot', fileFilter='magna-setvel')
+plot_timeseries(Files, 'PoseY', Title='Consolidated Plot', fileFilter='magna-setvel')
+
+print("Bag files captured in this run are:")
+print(Files)
 
 
