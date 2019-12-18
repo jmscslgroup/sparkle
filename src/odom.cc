0598b9725201608bce6dd64c0f55174206d4e68a
Rahul Bhadani <rahulbhadani@email.arizona.edu>
2019-12-17 21:57:16 -0700

    Completely rewrote the odometry model plugin: uses Model.CC Gazebo APIs to retrieved odometry data instead of querying from Gazebo-ROS topic

--- src/odom.cc
+++ src/odom.cc
@@ -1,154 +1,151 @@
 /*---------------------------------------------------
 Author: Rahul Bhadani
-Date: July 2019
+Date: December 2019
 
 This class implements a Gazebo plugin that publishes 
 odometry information for Sparkle car model
 
 ---------------------------------------------------*/
-
-#include <boost/bind.hpp>
+#include<ros/ros.h>
 #include <gazebo/gazebo.hh>
 #include <gazebo/physics/physics.hh>
 #include <gazebo/common/common.hh>
-#include <cstdio>
-#include "ros/ros.h"
-#include "geometry_msgs/Twist.h"
-
-#include <cstdlib>
-#include <stdlib.h>
-#include <pwd.h>
-#include <stdio.h>
-#include <sys/types.h>
-#include<iostream>
-#include<string>
-#include<vector>
-#include<ros/ros.h>
-#include<geometry_msgs/Twist.h>
-#include<nav_msgs/Odometry.h>
-#include "std_msgs/String.h"
+#include <boost/bind.hpp>
 #include <tf/transform_broadcaster.h>
-#include <gazebo_msgs/ModelState.h>
-#include <gazebo_msgs/ModelStates.h>
 
+#include <ignition/math/Box.hh>
+#include <ignition/math/Pose3.hh>
+#include <ignition/math/Vector3.hh>
+#include <ignition/math/Quaternion.hh>
+#include<nav_msgs/Odometry.h>
+
+#include<iostream>
+#include<cstdlib>
+#include<string>
 using namespace std;
 
 namespace gazebo
 {
-    class SparkleOdom: public ModelPlugin
+    class odometry: public ModelPlugin
     {
-        private: 
-            //to read the name space from urdf file
-            std::string robotNamespace;
+        private:
 
-            std::string tfScope;
-            // Name of the odometry topic being published
-            std::string odomTopic;
+            string model_name; //Save the model name 
+            string odometry_topic; // On what topic I should publish model's odometry data
+            string tf_scope; 
+
+            string model_state_topic; // What is my model state topic?
             
-            ros::Publisher odom_pub;
-            ros::Subscriber sub;
-            boost::thread ros_spinner_thread_;
-            ros::NodeHandle* rosnode_;
-            nav_msgs::Odometry odom;
-            //Gazebo
+            physics::ModelPtr model; // Model Pointer
+            ignition::math::Box bounding_box_model; //Bounding Box of the model
+            ignition::math::Box collision_box_model; // Bounding Box of the collision of the model
+            ignition::math::Pose3d model_poses;
+            ignition::math::Vector3<double> model_linear_vel;
+            ignition::math::Vector3<double> model_angular_vel;
             event::ConnectionPtr updateConnection;
-            //Pointer to the model entity
-            physics::ModelPtr model;
-            //Pointer to the world in which the model exists
-            physics::WorldPtr world;
-            ros::Time current_time;
-    
-        public:
-            SparkleOdom()
-            {
-                this->robotNamespace = "";
-                this->tfScope = "";
-                this->odomTopic = "";
             
-            }
-            //Destructor
-            ~SparkleOdom()
-            {
-                //Kill the boost thread
-                pthread_kill(this->ros_spinner_thread_.native_handle(), 9);
-                
-                this->rosnode_->shutdown();
-                delete this->rosnode_;
+            ros::Publisher odometry_publisher;
+            ros::NodeHandle* rosnode;
 
+            
+        public:
+            odometry()
+            {
+                model_name="";
+                odometry_topic="/odom";
+                
             }
             void Load(physics::ModelPtr _parent, sdf::ElementPtr _sdf)
             {
+                if(!ros::isInitialized)
+                {
+                    ROS_FATAL_STREAM("ROS node for Gazebo not established. Plugin failed.");
+                    return;
+                }
+
+                ROS_INFO_STREAM("Odometry ModelPlugin Loaded.");
                 this->model = _parent;
-                this->world = _parent->GetWorld();
-                this->robotNamespace = "";
-                
-                if(_sdf->HasElement("robotNamespace"))
-                {
-                    this->robotNamespace = _sdf->GetElement("robotNamespace")->Get<std::string>();
-                }
-                ROS_INFO_STREAM("Robot loaded is "<<this->robotNamespace);
-                this->odomTopic = this->robotNamespace + "/odom";
-                this->tfScope = this->robotNamespace.substr(1,this->robotNamespace.size()-1);
-                
-                //Start up ros_node
-                int argc = 0;
-                char** argv = NULL;
-                ros::init(argc, argv, "SparkleOdom", ros::init_options::NoSigintHandler |  ros::init_options::AnonymousName);
-                rosnode_ = new ros::NodeHandle(this->robotNamespace);
 
-                odom_pub = rosnode_->advertise<nav_msgs::Odometry>(odomTopic, 1);
+                this->model_name = this->model->GetName();
+                this->tf_scope = this->model_name;
+                this->odometry_topic = "/" + this->model_name + "/odom";
+                ROS_INFO_STREAM("Name of the model is "<< this->model_name);
+                ROS_INFO_STREAM("Odometry topic set is  "<< this->odometry_topic);
+               
+                this->rosnode = new ros::NodeHandle(this->model_name);
+                this->odometry_publisher =  this->rosnode->advertise<nav_msgs::Odometry>(odometry_topic, 1);
+
+                this->bounding_box_model = this->model->BoundingBox();
+                this->collision_box_model = this->model->CollisionBoundingBox();
+
+                ROS_INFO_STREAM("Bounding box of "<<this->model_name<<"= X:"<<this->bounding_box_model.XLength()
+                        <<"Y:"<<this->bounding_box_model.YLength()
+                        <<"Z:"<<this->bounding_box_model.ZLength());
+
+                ROS_INFO_STREAM("Collision Bounding box of "<<this->model_name<<"= X:"<<this->collision_box_model.XLength()
+                        <<"Y:"<<this->collision_box_model.YLength()
+                        <<"Z:"<<this->collision_box_model.ZLength());
                 
-                this->ros_spinner_thread_ = boost::thread( boost::bind( &SparkleOdom::OdomThread, this ) );
+                // Listen to the update event. This event is broadcast every simulation iteration.
+                this->updateConnection = event::Events::ConnectWorldUpdateBegin(boost::bind(&odometry::OnUpdate, this));
             }
-            private:
-                void OdomThread()
-                {
-                    ROS_INFO_STREAM("Sparkle Odom Publisher: Callback thread id=" << boost::this_thread::get_id());
-                    this->sub = rosnode_->subscribe("/gazebo/model_states",1, &SparkleOdom::getOdom, this);
+            
+            // Called by the world update start event
+            public: void OnUpdate()
+            {
+                ros::Time current_time = ros::Time::now();
+                this->model_poses = this->model->RelativePose();
+                this->model_linear_vel = this->model->RelativeLinearVel();
+                this->model_angular_vel = this->model->RelativeAngularVel ();
 
-                    ROS_INFO_STREAM("Subscriber has topic " << this->sub.getTopic() ); 
+                ignition::math::Vector3<double> pos = this->model_poses.Pos();
+                ignition::math::Quaternion<double> quat = this->model_poses.Rot();
 
-                    ros::Rate loop_rate(100);
-                    while (this->rosnode_->ok())
-                    {
-                        ros::spinOnce();
-                        loop_rate.sleep( );
-                    }
-                }
-                void getOdom(const gazebo_msgs::ModelStates::ConstPtr& msg)
-                {
-                    this->current_time = ros::Time::now();
-                    vector<string> modelNames = msg->name;
-                    
-                   // ROS_INFO_STREAM("Size: "<< modelNames.size() );
-                    int index=-1;
+                ROS_DEBUG_STREAM(this->model_name<<" Pos = X: "<<pos.X() <<" Y: "<<pos.Y() <<" Z: "<<pos.Z());
+                ROS_DEBUG_STREAM(this->model_name<<" Quat = X: "<<quat.X() <<" Y: "<<quat.Y() <<" Z: "<<quat.Z() << "W: "<<quat.W());
+                
+                ROS_DEBUG_STREAM(this->model_name<<" Linear Vel = X: "<<this->model_linear_vel.X() <<" Y: "<<this->model_linear_vel.Y() <<" Z: "<<this->model_linear_vel.Z());
+                ROS_DEBUG_STREAM(this->model_name<<" Angular = X: "<<this->model_angular_vel.X() <<" Y: "<<this->model_angular_vel.Y() <<" Z: "<<this->model_angular_vel.Z());
+                
+                static tf::TransformBroadcaster br;
+                tf::Transform transform;
+                transform.setOrigin( tf::Vector3(pos.X(),
+                            pos.Y(),
+                            pos.Z()));
+                tf::Quaternion q(quat.X(),
+                        quat.Y(),
+                        quat.Z(),
+                        quat.W());
+                    transform.setRotation(q);
+                    br.sendTransform(tf::StampedTransform(transform, current_time,
+                                this->tf_scope + "/odom",
+                                this->tf_scope + "/base_link") );
 
-                    // figure out which index we are in the msg list of model states
-                    for( int i=0; i<modelNames.size() && index<0; i++ )
-                    {
-                        if( this->robotNamespace == std::string("/"+modelNames[i]) )
-                        {
-                            index = i;
-                            break;
-                        }
-                    }
-                    if( index == -1 )
-                    {
-                        ROS_ERROR_STREAM("Unable to find odometry for model name " << this->robotNamespace << "=" <<       index);
-                    }
-                    else
-                    {                 
-                        // grab the odometry from the incoming msg and post it
-                        this->odom.header.stamp = this->current_time;
-                        this->odom.header.frame_id = this->tfScope + "/odom";
-                        // we calculate our index in the pose msg by 
-                        this->odom.child_frame_id = this->tfScope + "/base_link";
-                        this->odom.pose.pose = msg->pose[index];
-                        this->odom.twist.twist = msg->twist[index];                
-                        this->odom_pub.publish(this->odom);
-                    }
-                }
+                // grab the odometry from the incoming msg and post it
+                nav_msgs::Odometry odom;
+                odom.header.stamp = current_time;
+                odom.header.frame_id = this->tf_scope + "/odom";
+                // we calculate our index in the pose msg by
+                odom.child_frame_id = this->tf_scope + "/base_link";
+                odom.pose.pose.position.x = pos.X();
+                odom.pose.pose.position.y = pos.Y();
+                odom.pose.pose.position.z = pos.Z();
+                odom.pose.pose.orientation.x = quat.X();
+                odom.pose.pose.orientation.y = quat.Y();
+                odom.pose.pose.orientation.z = quat.Z();
+                odom.pose.pose.orientation.w = quat.W();
+
+                odom.twist.twist.linear.x = this->model_linear_vel.X();
+                odom.twist.twist.linear.y = this->model_linear_vel.Y();
+                odom.twist.twist.linear.z = this->model_linear_vel.Z();
+                odom.twist.twist.angular.x = this->model_angular_vel.X();
+                odom.twist.twist.angular.y = this->model_angular_vel.Y();
+                odom.twist.twist.angular.z = this->model_angular_vel.Z();
+
+                odometry_publisher.publish(odom);
+            }
+        
     };
-    GZ_REGISTER_MODEL_PLUGIN(SparkleOdom)
-    
+    // Register this plugin with the simulator
+    GZ_REGISTER_MODEL_PLUGIN(odometry)
 }
