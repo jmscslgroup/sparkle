042ddaaeddb99bace815998cf78162b613642eef
Rahul Bhadani <rahulbhadani@email.arizona.edu>
2019-12-18 17:02:51 -0700

    reverted back to older version with some changes

--- src/circle_impl.py
+++ src/circle_impl.py
@@ -4,9 +4,12 @@
 # Copyright (c) 2019 Rahul Bhadani, Arizona Board of Regents
 # All rights reserved.
 
-from Bagplot import plot_timeseries
-
+from circle import circle
+import Bagplot
+from Bagplot import Bagplot
 import signal
+import matlab.engine
+import pandas as pd
 import sys, math, time
 import matplotlib.pyplot as pt
 import matplotlib.animation as animation
@@ -14,52 +17,50 @@ from matplotlib import style
 import yaml
 from operator import add
 
-from api import sparkle_sim
 
 Files = []
+i = 1
+NUM_VEHICLES  = [1]
+UPDATE_RATE = [25]
 
-print("======   Simulation  Begin   =======")
+# A set of six simulation with two cars and update rate varying from 20 to 120 Hz
+for nn in NUM_VEHICLES: 
+    for i in UPDATE_RATE:
+        print("===================================")
+        print("========   Simulation  Begin   ========")
+        print("===================================")
 
-simulation_description = "Verifying repeatablity if simulation in absence and presence of the laser with varying number of vehicles and different configuration"
+        ## Simulation 1
+        # Define Simulation Configuration
+        simConfig = {"circumference": 260.0, "num_vehicle":  nn, "update_rate": i, "log_time": 90.0, "max_update_rate": 100.0, "time_step": 0.01, "include_laser": True, "description": "Trial"}
+        #cl = circle(simConfig["circumference"], simConfig["num_vehicle"])
+        Circ = circle(**simConfig)
+        #Print the X coordinates of all vehicles for sanity checking
+        print("X coordinates of vehicles spawned: ",  Circ.X)
 
-# datafiles = sparkle_sim(num_vehicles=1, publish_rate= 1.0, max_update_rate=25, time_step=0.01,  log_time= 120.0, include_laser=False, description=simulation_description)
-# Files.append(datafiles)
+        #Print the Y coordinates of all vehicles for sanity checking
+        print("Y coordinates of vehicles spawned: ",  Circ.Y)
 
-# datafiles = sparkle_sim(num_vehicles=1, publish_rate= 1.0, max_update_rate=25, time_step=0.01,  log_time= 120.0, include_laser=True, description=simulation_description)
-#Files.append(datafiles)
+        # Start a simulation
+        bagFile = Circ.start_circle_sim()
 
-# datafiles = sparkle_sim(num_vehicles=10, publish_rate= 1.0, max_update_rate=25, time_step=0.01,  log_time= 120.0, include_laser=False, description=simulation_description)
-#Files.append(datafiles)
+        if bagFile is not None:
+            Bag  = Bagplot(bagFile)
+            datafiles = Bag.getDataFile(fileFilter="magna-setvel", msg_types = "odom")
+            Files.append(datafiles)
+            Bag.plot_timeseries(datafiles, 'PoseY', fileFilter='magna-setvel')
 
-# datafiles = sparkle_sim(num_vehicles=10, publish_rate= 1.0, max_update_rate=25, time_step=0.01,  log_time= 120.0, include_laser=True, description=simulation_description)
-#Files.append(datafiles)
+        configFileToSave =  Circ.bagfile[0:-4] + "/" + "simConfig.yaml"
 
-datafiles = sparkle_sim(num_vehicles=10, publish_rate= 10.0, max_update_rate=25, time_step=0.01,  log_time= 120.0, include_laser=False,description=simulation_description)
-Files.append(datafiles)
-
-datafiles = sparkle_sim(num_vehicles=10, publish_rate= 10.0, max_update_rate=25, time_step=0.01,  log_time= 120.0, include_laser=True, description=simulation_description)
-Files.append(datafiles)
-
-# datafiles = sparkle_sim(num_vehicles=25, publish_rate= 10.0, max_update_rate=25, time_step=0.01,  log_time= 120.0, include_laser=False, description=simulation_description)
-#Files.append(datafiles)
-
-# datafiles = sparkle_sim(num_vehicles=25, publish_rate= 10.0, max_update_rate=25, time_step=0.01,  log_time= 120.0, include_laser=True, description=simulation_description)
-#Files.append(datafiles)
-
-
-# datafiles = sparkle_sim(num_vehicles=25, publish_rate= 50.0, max_update_rate=25, time_step=0.01,  log_time= 120.0, include_laser=False, description=simulation_description)
-#Files.append(datafiles)
-
-
-# datafiles = sparkle_sim(num_vehicles=25, publish_rate= 50.0, max_update_rate=25, time_step=0.01,  log_time= 120.0, include_laser=True,description=simulation_description)
-#Files.append(datafiles)
-
+        with open(configFileToSave, 'w') as file:
+            documents = yaml.dump(simConfig, file)
+    
+        time.sleep(7)
 
 # Now since we have all the bag files
 
 Files = reduce(add, Files)
 
-plot_timeseries(Files, 'PoseY', Title='Consolidated Plot', fileFilter='magna-setvel')
+Bagplot.plot_timeseries(Files, 'PoseY', Title='Consolidated Plot', fileFilter='magna-setvel')
 
-print("Bag files captured in this run are:")
-print(Files)
+
