b167a77fb1b456a86bc5e11f86e455e29c8fccba
Rahul Bhadani <rahulbhadani@email.arizona.edu>
2019-12-19 01:50:07 -0700

    Various API call for simulation testing

--- src/api_call.py
+++ src/api_call.py
@@ -4,54 +4,55 @@
 # Copyright (c) 2019 Rahul Bhadani, Arizona Board of Regents
 # All rights reserved.
 
-from Bagplot import plot_timeseries
 from api import sparkle_sim
+from operator import add
+from Bagplot import plot_ts
 
 Files = []
 
-datafile = sparkle_sim(num_vehicles=2, publish_rate=1, max_update_rate=25, time_step=0.01, log_time=360, include_laser=True, description="API Testing")
+datafile = sparkle_sim(num_vehicles=2, publish_rate=1, max_update_rate=25, time_step=0.01, log_time=300, include_laser=True, description="API Testing")
 Files.append(datafile)
 print("Data received is: {}".format(datafile))
 
-datafile = sparkle_sim(num_vehicles=2, publish_rate=1, max_update_rate=25, time_step=0.01, log_time=360, include_laser=False, description="API Testing")
+datafile = sparkle_sim(num_vehicles=2, publish_rate=1, max_update_rate=25, time_step=0.01, log_time=300, include_laser=False, description="API Testing")
 Files.append(datafile)
 print("Data received is: {}".format(datafile))
 
-datafile = sparkle_sim(num_vehicles=10, publish_rate=1, max_update_rate=25, time_step=0.01, log_time=360, include_laser=True, description="API Testing")
+datafile = sparkle_sim(num_vehicles=10, publish_rate=1, max_update_rate=25, time_step=0.01, log_time=300, include_laser=True, description="API Testing")
 Files.append(datafile)
 print("Data received is: {}".format(datafile))
 
-datafile = sparkle_sim(num_vehicles=10, publish_rate=1, max_update_rate=25, time_step=0.01, log_time=360, include_laser=False, description="API Testing")
+datafile = sparkle_sim(num_vehicles=10, publish_rate=1, max_update_rate=25, time_step=0.01, log_time=300, include_laser=False, description="API Testing")
 Files.append(datafile)
 print("Data received is: {}".format(datafile))
 
-datafile = sparkle_sim(num_vehicles=10, publish_rate=25, max_update_rate=25, time_step=0.01, log_time=360, include_laser=True, description="API Testing")
+datafile = sparkle_sim(num_vehicles=10, publish_rate=25, max_update_rate=25, time_step=0.01, log_time=300, include_laser=True, description="API Testing")
 Files.append(datafile)
 print("Data received is: {}".format(datafile))
 
-datafile = sparkle_sim(num_vehicles=10, publish_rate=25, max_update_rate=25, time_step=0.01, log_time=360, include_laser=False, description="API Testing")
+datafile = sparkle_sim(num_vehicles=10, publish_rate=25, max_update_rate=25, time_step=0.01, log_time=300, include_laser=False, description="API Testing")
 Files.append(datafile)
 print("Data received is: {}".format(datafile))
 
-datafile = sparkle_sim(num_vehicles=25, publish_rate=1, max_update_rate=25, time_step=0.01, log_time=360, include_laser=True, description="API Testing")
+datafile = sparkle_sim(num_vehicles=25, publish_rate=1, max_update_rate=25, time_step=0.01, log_time=300, include_laser=True, description="API Testing")
 Files.append(datafile)
 print("Data received is: {}".format(datafile))
 
-datafile = sparkle_sim(num_vehicles=25, publish_rate=1, max_update_rate=25, time_step=0.01, log_time=360, include_laser=False, description="API Testing")
+datafile = sparkle_sim(num_vehicles=25, publish_rate=1, max_update_rate=25, time_step=0.01, log_time=300, include_laser=False, description="API Testing")
 Files.append(datafile)
 print("Data received is: {}".format(datafile))
 
-datafile = sparkle_sim(num_vehicles=25, publish_rate=25, max_update_rate=25, time_step=0.01, log_time=360, include_laser=True, description="API Testing")
+datafile = sparkle_sim(num_vehicles=25, publish_rate=25, max_update_rate=25, time_step=0.01, log_time=300, include_laser=True, description="API Testing")
 Files.append(datafile)
 print("Data received is: {}".format(datafile))
 
-datafile = sparkle_sim(num_vehicles=25, publish_rate=25, max_update_rate=25, time_step=0.01, log_time=360, include_laser=False, description="API Testing")
+datafile = sparkle_sim(num_vehicles=25, publish_rate=25, max_update_rate=25, time_step=0.01, log_time=300, include_laser=False, description="API Testing")
 Files.append(datafile)
 print("Data received is: {}".format(datafile))
 
 Files = reduce(add, Files)
 
-plot_timeseries(Files, 'PoseY', Title='Consolidated Plot', fileFilter='magna-setvel')
+plot_ts(Files, 'PoseY', Title='Consolidated Plot', fileFilter='magna-setvel')
 
 print("Bag files captured in this run are:")
 print(Files)
