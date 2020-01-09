076710355fba11817a1970745e85dfa03b265f22
Rahul Bhadani <rahulbhadani@email.arizona.edu>
2020-01-08 20:43:55 -0700

    API CALL example changed

--- src/api_call.py
+++ src/api_call.py
@@ -10,45 +10,57 @@ from Bagplot import plot_ts
 
 Files = []
 
-datafile = sparkle_sim(num_vehicles=2, publish_rate=1, max_update_rate=25, time_step=0.01, log_time=300, include_laser=True, description="API Testing")
+datafile = sparkle_sim(num_vehicles=30, publish_rate=100, max_update_rate=50, time_step=0.01, log_time=400, include_laser=True, description="API Testing")
 Files.append(datafile)
 print("Data received is: {}".format(datafile))
 
-datafile = sparkle_sim(num_vehicles=2, publish_rate=1, max_update_rate=25, time_step=0.01, log_time=300, include_laser=False, description="API Testing")
-Files.append(datafile)
-print("Data received is: {}".format(datafile))
+# datafile = sparkle_sim(num_vehicles=30, publish_rate=100, max_update_rate=50, time_step=0.01, log_time=400, include_laser=True, description="API Testing")
+# Files.append(datafile)
+# print("Data received is: {}".format(datafile))
 
-datafile = sparkle_sim(num_vehicles=10, publish_rate=1, max_update_rate=25, time_step=0.01, log_time=300, include_laser=True, description="API Testing")
-Files.append(datafile)
-print("Data received is: {}".format(datafile))
+# datafile = sparkle_sim(num_vehicles=30, publish_rate=1, max_update_rate=100, time_step=0.01, log_time=400, include_laser=True, description="API Testing")
+# Files.append(datafile)
+# print("Data received is: {}".format(datafile))
 
-datafile = sparkle_sim(num_vehicles=10, publish_rate=1, max_update_rate=25, time_step=0.01, log_time=300, include_laser=False, description="API Testing")
-Files.append(datafile)
-print("Data received is: {}".format(datafile))
+# datafile = sparkle_sim(num_vehicles=30, publish_rate=100, max_update_rate=100, time_step=0.01, log_time=400, include_laser=True, description="API Testing")
+# Files.append(datafile)
+# print("Data received is: {}".format(datafile))
 
-datafile = sparkle_sim(num_vehicles=10, publish_rate=25, max_update_rate=25, time_step=0.01, log_time=300, include_laser=True, description="API Testing")
-Files.append(datafile)
-print("Data received is: {}".format(datafile))
+# datafile = sparkle_sim(num_vehicles=2, publish_rate=1, max_update_rate=25, time_step=0.01, log_time=300, include_laser=False, description="API Testing")
+# Files.append(datafile)
+# print("Data received is: {}".format(datafile))
 
-datafile = sparkle_sim(num_vehicles=10, publish_rate=25, max_update_rate=25, time_step=0.01, log_time=300, include_laser=False, description="API Testing")
-Files.append(datafile)
-print("Data received is: {}".format(datafile))
+# datafile = sparkle_sim(num_vehicles=10, publish_rate=1, max_update_rate=25, time_step=0.01, log_time=300, include_laser=True, description="API Testing")
+# Files.append(datafile)
+# print("Data received is: {}".format(datafile))
 
-datafile = sparkle_sim(num_vehicles=25, publish_rate=1, max_update_rate=25, time_step=0.01, log_time=300, include_laser=True, description="API Testing")
-Files.append(datafile)
-print("Data received is: {}".format(datafile))
+# datafile = sparkle_sim(num_vehicles=10, publish_rate=1, max_update_rate=25, time_step=0.01, log_time=300, include_laser=False, description="API Testing")
+# Files.append(datafile)
+# print("Data received is: {}".format(datafile))
 
-datafile = sparkle_sim(num_vehicles=25, publish_rate=1, max_update_rate=25, time_step=0.01, log_time=300, include_laser=False, description="API Testing")
-Files.append(datafile)
-print("Data received is: {}".format(datafile))
+# datafile = sparkle_sim(num_vehicles=10, publish_rate=25, max_update_rate=25, time_step=0.01, log_time=300, include_laser=True, description="API Testing")
+# Files.append(datafile)
+# print("Data received is: {}".format(datafile))
 
-datafile = sparkle_sim(num_vehicles=25, publish_rate=25, max_update_rate=25, time_step=0.01, log_time=300, include_laser=True, description="API Testing")
-Files.append(datafile)
-print("Data received is: {}".format(datafile))
+# datafile = sparkle_sim(num_vehicles=10, publish_rate=25, max_update_rate=25, time_step=0.01, log_time=300, include_laser=False, description="API Testing")
+# Files.append(datafile)
+# print("Data received is: {}".format(datafile))
 
-datafile = sparkle_sim(num_vehicles=25, publish_rate=25, max_update_rate=25, time_step=0.01, log_time=300, include_laser=False, description="API Testing")
-Files.append(datafile)
-print("Data received is: {}".format(datafile))
+# datafile = sparkle_sim(num_vehicles=25, publish_rate=1, max_update_rate=25, time_step=0.01, log_time=300, include_laser=True, description="API Testing")
+# Files.append(datafile)
+# print("Data received is: {}".format(datafile))
+
+# datafile = sparkle_sim(num_vehicles=25, publish_rate=1, max_update_rate=25, time_step=0.01, log_time=300, include_laser=False, description="API Testing")
+# Files.append(datafile)
+# print("Data received is: {}".format(datafile))
+
+# datafile = sparkle_sim(num_vehicles=25, publish_rate=25, max_update_rate=25, time_step=0.01, log_time=300, include_laser=True, description="API Testing")
+# Files.append(datafile)
+# print("Data received is: {}".format(datafile))
+
+# datafile = sparkle_sim(num_vehicles=25, publish_rate=25, max_update_rate=25, time_step=0.01, log_time=300, include_laser=False, description="API Testing")
+# Files.append(datafile)
+# print("Data received is: {}".format(datafile))datafile
 
 Files = reduce(add, Files)
 
