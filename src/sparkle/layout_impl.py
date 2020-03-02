de0ad150ffd3e435dcb00dfc20e427f1bfa797e1
Rahul Bhadani <rahulbhadani@email.arizona.edu>
2020-03-02 00:01:47 -0700

    A Simple Example of layout class instantiation

---
+++ src/sparkle/layout_impl.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+#
+# Author: Rahul Bhadani
+# Copyright (c) 2019-2020 Rahul Bhadani, Arizona Board of Regents
+# All rights reserved.
+from layout import layout
+
+L = layout()
