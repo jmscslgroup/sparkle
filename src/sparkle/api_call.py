cc1f61f435aeb6c146fe74649620d041371d65e8
Rahul Bhadani <rahulbhadani@email.arizona.edu>
2020-03-05 10:01:45 -0700

    Modified example for sparkle

--- src/sparkle/api_call.py
+++ src/sparkle/api_call.py
@@ -7,12 +7,41 @@
 from api import animate_circle
 from operator import add
 from Bagplot import plot_ts
+import uuid
+import matplotlib.pyplot as plt
+plt.style.use('ggplot')
+plt.rcParams["font.family"] = "Times New Roman"
+import pandas as pd
 
 Files = []
+unique_runid = str(uuid.uuid4())
+vehicle = 5
+for i in range(4):
+    datafile = animate_circle(circumference = 230, n_vehicles=vehicle, publish_rate=100.0, max_update_rate=100.0, time_step=0.01, log_time=300.0, include_laser=False, logdir = "/home/ivory/CyverseData/ProjectSparkle", description="CAT Vehicle Simulation:" + str(vehicle) + "Car, Iteration " + str(i) + " ID " + unique_runid)
+    Files.append(datafile)
+    print("Data received is: {}".format(datafile))
 
-datafile = animate_circle(circumference = 230, num_vehicles=1, publish_rate=100.0, max_update_rate=100.0, time_step=0.01, log_time=20.0, include_laser=True, logdir = "/home/ivory/CyverseData/ProjectSparkle", description="Testing Sparkle Simulation.")
-Files.append(datafile)
-print("Data received is: {}".format(datafile))
+Files = reduce(add, Files)
+
+plot_ts(Files, 'PoseY', Title='/home/ivory/CyverseData/ProjectSparkle/Consolidated Plot', fileFilter='magna-setvel')
+
+print("Bag files captured in this run are:")
+print(Files)
+
+
+# Odom dataframe 
+fig, ax = plt.subplots(1,1)
+fig.tight_layout(pad=5.0)
+for data in Files:
+    odom_df = pd.read_csv(data)
+    plt.plot(odom_df['PoseX'],  odom_df['PoseY'], linewidth = 1, label = data)
+
+plt.title('Trajectory of CAT Vehicle During Simulation', fontsize= 24)
+ax.set_xlabel('x',fontsize=24)
+ax.set_ylabel('y', fontsize=24)
+plt.legend()
+plt.show()
+
 
 # datafile = animate_circle(num_vehicles=30, publish_rate=100, max_update_rate=50, time_step=0.01, log_time=400, include_laser=True, description="API Testing")
 # Files.append(datafile)
@@ -62,9 +91,3 @@ print("Data received is: {}".format(datafile))
 # Files.append(datafile)
 # print("Data received is: {}".format(datafile))datafile
 
-Files = reduce(add, Files)
-
-# plot_ts(Files, 'PoseY', Title='Consolidated Plot', fileFilter='magna-setvel')
-
-print("Bag files captured in this run are:")
-print(Files)
