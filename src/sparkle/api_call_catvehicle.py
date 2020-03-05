57b0e580586377608ad3fce36c32ecbe6ee38bdf
Rahul Bhadani <rahulbhadani@email.arizona.edu>
2020-03-05 00:32:56 -0700

    Legend added to the plot

--- src/sparkle/api_call_catvehicle.py
+++ src/sparkle/api_call_catvehicle.py
@@ -13,21 +13,22 @@ import pandas as pd
 
 Files = []
 
-for i in range(1):
-    datafile = animate_catvehicle( package_name="catvehicle", circumference = 230, num_vehicles=1, publish_rate=100.0, max_update_rate=100.0, time_step=0.01, log_time=20.0, include_laser=True, logdir = "/home/ivory/CyverseData/ProjectSparkle", description="CAT Vehice Simulation: Single Car")
+for i in range(4):
+    datafile = animate_catvehicle( package_name="catvehicle", circumference = 230, num_vehicles=1, publish_rate=100.0, max_update_rate=100.0, time_step=0.01, log_time=30.0, include_laser=True, logdir = "/home/ivory/CyverseData/ProjectSparkle", description="CAT Vehice Simulation: Single Car")
     Files.append(datafile)
     print("Data received is: {}".format(datafile))
 
-
+Files = reduce(add, Files)
 # Odom dataframe 
 fig, ax = plt.subplots(1,1)
 fig.tight_layout(pad=5.0)
-for data in datafile:
+for data in Files:
     odom_df = pd.read_csv(data)
-    plt.plot(odom_df['PoseX'],  odom_df['PoseY'], linewidth = 1)
+    plt.plot(odom_df['PoseX'],  odom_df['PoseY'], linewidth = 1, label = data)
 
 plt.title('Trajectory of CAT Vehicle During Simulation', fontsize= 24)
 ax.set_xlabel('x',fontsize=24)
 ax.set_ylabel('y', fontsize=24)
+plt.legend()
 plt.show()
 
