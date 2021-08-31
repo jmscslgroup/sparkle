08265811e1751a1a0edd38a872833b0313e8b5c3
Rahul Bhadani <rahulbhadani@email.arizona.edu>
2021-08-30 21:53:41 -0700

    Moved to sparkle python repo

--- src/sparkle/layout_impl.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-#
-# Author: Rahul Bhadani
-# Initial Date: February 2020
-# Copyright (c) Rahul Bhadani, Arizona Board of Regents
-# All rights reserved.
-from .layout import layout
-
-L = layout()
