bccaf5c0d8a7f93e9ef37c10e10ef1f002315294
Rahul Bhadani <rahulbhadani@email.arizona.edu>
2021-10-03 17:58:58 -0700

    Moved unused function definition to a new file

--- src/model_state.cc
+++ src/model_state.cc
@@ -23,32 +23,9 @@
 #include<ignition/math/Vector3.hh>
 #include<ignition/math/Box.hh>
 
+#include "mymath.cc"
 using namespace std;
 
-// Convert ROLL PITCH YAW to QUETERNION
-geometry_msgs::Quaternion rpyToQuaternion(double roll, double pitch, double yaw)
-{
-    double cy = cos(yaw * 0.5);
-    double sy = sin(yaw * 0.5);
-    double cp = cos(pitch * 0.5);
-    double sp = sin(pitch * 0.5);
-    double cr = cos(roll * 0.5);
-    double sr = sin(roll * 0.5);
-
-
-    double w = cy * cp * cr + sy * sp * sr;
-    double x = cy * cp * sr - sy * sp * cr;
-    double y = sy * cp * sr + cy * sp * cr;
-    double z = sy * cp * cr - cy * sp * sr;
-
-    geometry_msgs::Quaternion quat;
-    quat.x = x;
-    quat.y = y;
-    quat.z = z;
-    quat.w = w;
-
-    return quat;
-}
 
 class SetModelPose
 {
